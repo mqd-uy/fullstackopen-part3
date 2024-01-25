1f943daf15c0d638ff6f32ab5243affb674b3317
Mauricio <mauri1987@gmail.com>
2024-01-25 20:43:48 -0300

    Exercise 3.20

--- models/Person.js
+++ models/Person.js
@@ -14,7 +14,15 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        validate: {
+            validator: (v) => {
+                if (v.length < 8)
+                    return false
+                const regex = /^\d{2,3}[-]\d+$/
+                return v.match(regex)
+            },
+            message: 'Phone number must be at least 8 digits. Prefix should be 2 o 3 digits. Prefix and number must be dash (-) separated'
+        }
     }
 })
 
